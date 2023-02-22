ea6b6a576dd172eaa1534f29329c7a5d6b0817a5
Eduardo Fuentes <95595866+EduardoFuentesRocha@users.noreply.github.com>
2023-02-22 16:54:28 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,14 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      Síguenos{' '}
+      <a href="https://instagram.com/lasmujereshablan_mx/">@lasmujereshablan_mx</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()} Todos los derechos reservados.
         </span>
       </div>
     </footer>
